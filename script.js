79501227062ad29878865de4a928b2e9032fe203
JoshTrev <joshj438t@gmail.com>
2020-09-21 20:58:08 -0400

    Updated portfolio with images and working links

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#img1").on("click" , function (event){
+    window.location.href = "https://joshtrev.github.io/BetterMe/"
+})
+
+$("#img2").on("click" , function (event){
+    window.location.href = "https://joshtrev.github.io/Weather-Dashboard/"
+})
+
+$("#img3").on("click" , function (event){
+    window.location.href = "https://joshtrev.github.io/PasswordGeneratorHomework/"
+})
